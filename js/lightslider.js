fd89a7d20afd83784fa77d98accb542abda53dab
SAKTHIVEL M <sakthivelm8973@gmail.com>
2023-10-28 20:44:56 +0530

    First Commit
    First Date:28/10/2023

---
+++ js/lightslider.js
@@ -0,0 +1,22 @@
+
+ /*----Full Page Slider---------------*/
+ 
+ $(document).ready(function() {
+    $('#adaptive').lightSlider({
+        adaptiveHeight:true,
+        auto:true,
+        item:1,
+        slideMargin:0,
+        loop:true
+    });
+});
+ /*--For-Product-SLider----------------*/
+ $(document).ready(function() {
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });  
+  });
